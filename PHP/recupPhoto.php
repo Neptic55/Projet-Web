2120c7d5f77bc918c6f0fe8a91136c29098491dd
tuturapeur55 <tuturesse@gmail.com>
2017-04-19 17:27:02 +0200

    album photo

--- PHP/recupPhoto.php
+++ PHP/recupPhoto.php
@@ -1,12 +1,13 @@
 <?php
 
 if(!empty($_FILES)) {
-
+    require("imgClass.php");
     $img = $_FILES['img'];
-    $ext = strtolower(substr($img['name'],-3,));
+    $ext = strtolower(substr($img['name'],-3));
     $allow_ext = array("jpg",'png','gif','jpeg');
     if(in_array($ext,$allow_ext)) {
-        move_uploaded_file($img['tmp_name'], "albums/" . $img['name']);
+        move_uploaded_file($img['tmp_name'], "../albums/general/" . $img['name']);
+        Img::creerMin("../albums/general/" . $img['name'],"../albums/general/miniature",$img['name'],215,112);
     }
     else {
         $erreur = "Votre fichier n'est pas une image";
@@ -15,5 +16,15 @@ if(!empty($_FILES)) {
 
 if(isset($erreur)){
     echo $erreur;
+}
+
+$dos = "image/min";
+$dir = opendir($dos);
+while ($file = readdir($dir)){
+    $allow_ext = array("jpg",'png','gif','jpeg');
+    $ext = strtolower(substr($file,-3));
+    if(in_array($ext,$allow_ext)) {
+        echo $file;
+    }
 }
 ?>
