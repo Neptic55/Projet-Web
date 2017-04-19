c4632d34043444906965c6b2d544ddccae09a05d
tuturapeur55 <tuturesse@gmail.com>
2017-04-19 16:39:01 +0200

    que fichier image

--- PHP/recupPhoto.php
+++ PHP/recupPhoto.php
@@ -2,8 +2,18 @@
 
 if(!empty($_FILES)) {
 
-    $img= $_FILES['img'];
-    move_uploaded_file($img['tmp_name'],"albums/".$img['name']);
+    $img = $_FILES['img'];
+    $ext = strtolower(substr($img['name'],-3,));
+    $allow_ext = array("jpg",'png','gif','jpeg');
+    if(in_array($ext,$allow_ext)) {
+        move_uploaded_file($img['tmp_name'], "albums/" . $img['name']);
+    }
+    else {
+        $erreur = "Votre fichier n'est pas une image";
+    }
+}
 
+if(isset($erreur)){
+    echo $erreur;
 }
 ?>
