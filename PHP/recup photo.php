3285ad4355340519c6c4540bb4c63b05f89a8383
tuturapeur55 <tuturesse@gmail.com>
2017-04-19 12:12:12 +0200

    recupphoto2

--- PHP/recup photo.php
+++ PHP/recup photo.php
@@ -5,7 +5,7 @@
  *************************************************************/
 
 // Constantes
-define('TARGET', '/files/');    // Repertoire cible
+define('TARGET', '/albums/');    // Repertoire cible
 define('MAX_SIZE', 100000);    // Taille max en octets du fichier
 define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
 define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
@@ -18,15 +18,6 @@ $infosImg = array();
 $extension = '';
 $message = '';
 $nomImage = '';
-
-/************************************************************
- * Creation du repertoire cible si inexistant
- *************************************************************/
-if( !is_dir(TARGET) ) {
-    if( !mkdir(TARGET, 0755) ) {
-        exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
-    }
-}
 
 /************************************************************
  * Script d'upload
